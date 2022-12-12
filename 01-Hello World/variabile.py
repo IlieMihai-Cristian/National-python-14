f351a72029b5b1915c96abb0274da346ad74b509
mihai <mihai.ilie@nxdata.ro>
2022-12-12 21:42:20 +0200

    variabile.py

---
+++ 01-Hello World/variabile.py
@@ -0,0 +1,41 @@
+# first_number = 4
+# firstNumber = first_number + 4
+# print(first_number)
+#
+# # 1asdasdasd = 3
+# # keywords = del, None, True, false, def, if, else, return,
+# # b = 4
+#
+# # opertori asignare:
+# first_number = first_number + 1
+# first_number += 1
+#
+# first_number -= 1
+# first_number /= 1
+
+# first_number = '1.10'
+# print(type(first_number))
+
+# integer (int), float, complex - numere
+# string (str)
+# list ( [1, 4, 'aaaa', True] ) / tuple / set / dictionary
+# boolean True/False
+# None
+
+
+# var_1 = 2
+# var_2 = "2"
+# print(var_1*var_2)
+
+# sa se dezvolte un program care sa calculeze venitul saptamanal al unui salariat:
+# venit_saptamanal = nr_de_ore * venit_ora
+# hourly_rate = 10.5
+# nr_of_hours = 40
+""" sadsanabdndksahd """
+strings = 'asdssadhsag'
+strings_1 = "asdssad\"hsag"
+
+hourly_rate = float(input('Introdu\' venit/ora'))
+nr_of_hours = float(input('Introdu\' nr. ore'))
+weekly_income = hourly_rate * nr_of_hours
+print('venit saptamanal = ', weekly_income)
