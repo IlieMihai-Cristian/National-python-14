4c204f37bd1206026e70337d6f371c1240cda0cd
mihai <mihai.ilie@nxdata.ro>
2023-03-08 21:54:14 +0200

    django-3

--- django_proiect/proiect/aplicatie1/urls.py
+++ django_proiect/proiect/aplicatie1/urls.py
@@ -5,4 +5,8 @@ app_name = 'locations'
 
 urlpatterns = [
     path('', views.LocationsView.as_view(), name='lista_locatii'),
+    path('adaugare/', views.CreateLocationsView.as_view(), name='adaugare'),
+    path('<int:pk>/update/', views.UpdateLocationsView.as_view(), name='modificare'),
+    path('<int:pk>/delete/', views.delete_location, name='stergere'),
+    path('<int:pk>/activate/', views.activate_location, name='activare'),
 ]
--- django_proiect/proiect/aplicatie1/views.py
+++ django_proiect/proiect/aplicatie1/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, CreateView, UpdateView
+from django.urls import reverse
 from aplicatie1.models import Location
 
 
@@ -13,3 +13,55 @@ from aplicatie1.models import Location
 class LocationsView(ListView):
     model = Location
     template_name = 'aplicatie1/locations_index.html'
+
+
+class CreateLocationsView(CreateView):
+    model = Location
+    template_name = 'aplicatie1/locations_form.html'
+    fields = ['city', 'country']
+
+    def get_success_url(self):
+        return reverse('locations:lista_locatii')
+
+
+class UpdateLocationsView(UpdateView):
+    model = Location
+    fields = ['city', 'country']
+    template_name = 'aplicatie1/locations_form.html'
+
+    def get_success_url(self):
+        return reverse('locations:lista_locatii')
+
+
+def delete_location(request, pk):
+    """
+    SQL native query: SELECT id, city FROM aplicatie1_location WHERE id=1
+    Django query: Location.objects.get(id=1).id
+
+    model.objects.get(coloana1=val1, coloana2=val2) -> returneaza o singura inregistrare
+                -> daca nu exista inregistrarea cautata in db sau exista mai multe inregistrari in db care contin
+                aceeasi valoare a parametrilor din get, va returna eroare (recomandat este sa fie folosit ca si param. id-ul)
+                -> avand in vedere ca returneaza o singura inregistrare (obiect) putem sa accesam direct atributul
+                (proprietatea, field-ul din db): model.objects.get(id=1).city
+    models.object.filter(coloana1=val1, coloana2=val2) -> returneaza 1 sau mai multe inregistrari
+                -> ca sa accesez atributele (field-urile) pot sa:
+                    - fie iteram prin rezultate
+                    - fie sa scriu model.objects.filter().values()
+                    - fie sa scriu model.objects.filter().values_list('city')
+    @type request:
+    @param pk:
+    """
+    # folosind get
+    # instance_object = Location.objects.get(id=pk)
+    # if instance_object.active:
+    #     instance_object.active = False
+    #     instance_object.save()
+
+    # folsind filter
+    Location.objects.filter(id=pk).update(active=False)
+    return redirect('locations:lista_locatii')
+
+
+def activate_location(request, pk):
+    Location.objects.filter(id=pk).update(active=True)
+    return redirect('locations:lista_locatii')
